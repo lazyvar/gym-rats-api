69f95f8e08baa9296b5c77bb71af17a4885d8c02
Mack Hasz <big.mack.with.pies@gmail.com>
2020-01-26 13:11:49 -0800

    shared queries in model repo

---
+++ lib/gym_rats/repos/challenge_repo.ex
@@ -0,0 +1,30 @@
+defmodule GymRats.Repo.ChallengeRepo do
+  import Ecto.Query, only: [where: 3]
+
+  alias GymRats.Repo
+  alias GymRats.Model.Challenge
+
+  defdelegate now, to: NaiveDateTime, as: :utc_now
+
+  def all do
+    Challenge |> Repo.all
+  end
+
+  def active do
+    Challenge
+    |> where([c], c.start_date < ^now() and c.end_date > ^now())
+    |> Repo.all
+  end
+
+  def complete do
+    Challenge
+    |> where([c], c.end_date < ^now())
+    |> Repo.all
+  end
+
+  def upcoming do
+    Challenge
+    |> where([c], c.start_date > ^now())
+    |> Repo.all
+  end
+end
--- lib/gym_rats_web/controllers/challenge_controller.ex
+++ lib/gym_rats_web/controllers/challenge_controller.ex
@@ -1,9 +1,22 @@
 defmodule GymRatsWeb.ChallengeController do
   use GymRatsWeb, :controller
-  
+
   alias GymRats.Model.Challenge
+  alias GymRats.Repo.ChallengeRepo
+
+  def index(conn, %{"filter" => filter}) do
+    challenges = case filter do
+      "all" -> ChallengeRepo.all
+      "active" -> ChallengeRepo.active
+      "complete" -> ChallengeRepo.complete
+      "upcoming" -> ChallengeRepo.upcoming
+      _ -> []
+    end
+    
+    success(conn, challenges)
+  end
 
   def index(conn, _params) do
-    success(conn, Repo.all(Challenge))
+    index(conn, %{"filter" => "all"})
   end
 end
