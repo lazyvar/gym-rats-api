63dac8477d6033e635eb2c066db34b7dfb88e708
Mack Hasz <big.mack.with.pies@gmail.com>
2020-01-26 10:23:44 -0800

    MessageStatus model

---
+++ lib/gym_rats/message_status.ex
@@ -0,0 +1,20 @@
+defmodule GymRats.MessageStatus do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "user_read_messages" do
+    field :read, :boolean, default: false
+    field :gym_rats_user_id, :integer
+    field :challenge_id, :integer
+    field :chat_message_id, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(message_status, attrs) do
+    message_status
+    |> cast(attrs, [:read])
+    |> validate_required([:read])
+  end
+end
