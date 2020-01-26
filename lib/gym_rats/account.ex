154e7cd80a392e28f7b0adaa40d59e50c42f25db
Mack Hasz <big.mack.with.pies@gmail.com>
2020-01-26 09:56:45 -0800

    account model

---
+++ lib/gym_rats/account.ex
@@ -0,0 +1,22 @@
+defmodule GymRats.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "gym_rats_users" do
+    field :email, :string
+    field :full_name, :string
+    field :password_digest, :string
+    field :profile_picture_url, :string
+    field :reset_password_token, :string
+    field :reset_password_token_expiration, :utc_datetime
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:full_name, :email, :profile_picture_url, :password_digest, :reset_password_token])
+    |> validate_required([:full_name, :email, :profile_picture_url, :password_digest, :reset_password_token])
+  end
+end
---
+++ priv/repo/migrations/20200126174221_create_gym_rats_users.exs
@@ -0,0 +1,18 @@
+defmodule GymRats.Repo.Migrations.CreateGymRatsUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:gym_rats_users) do
+      add :full_name, :string, null: false
+      add :email, :string, null: false
+      add :profile_picture_url, :string
+      add :password_digest, :string, null: false
+      add :reset_password_token, :string
+      add :reset_password_token_expiration, :utc_datetime
+
+      timestamps()
+    end
+
+    create unique_index(:gym_rats_users, [:email])
+  end
+end
