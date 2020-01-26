b2f476f33212355ad49b9e5bfeda83fa50ba228c
Mack Hasz <big.mack.with.pies@gmail.com>
2020-01-26 10:16:29 -0800

    workout model

---
+++ lib/gym_rats/workout.ex
@@ -0,0 +1,27 @@
+defmodule GymRats.Workout do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "workouts" do
+    field :calories, :integer
+    field :challenge_id, :integer
+    field :description, :string
+    field :distance, :string
+    field :duration, :integer
+    field :google_place_id, :string
+    field :gym_rats_user_id, :integer
+    field :photo_url, :string
+    field :points, :integer
+    field :steps, :integer
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(workout, attrs) do
+    workout
+    |> cast(attrs, [:gym_rats_user_id, :title, :description, :photo_url, :google_place_id, :challenge_id, :duration, :distance, :steps, :calories, :points])
+    |> validate_required([:gym_rats_user_id, :title, :description, :photo_url, :google_place_id, :challenge_id, :duration, :distance, :steps, :calories, :points])
+  end
+end
---
+++ priv/repo/migrations/20200126181431_create_workouts.exs
@@ -0,0 +1,24 @@
+defmodule GymRats.Repo.Migrations.CreateWorkouts do
+  use Ecto.Migration
+
+  def change do
+    create table(:workouts) do
+      add :gym_rats_user_id, :bigint, null: false
+      add :title, :string, null: false
+      add :description, :string
+      add :photo_url, :string
+      add :google_place_id, :string
+      add :challenge_id, :integer
+      add :duration, :integer
+      add :distance, :string
+      add :steps, :integer
+      add :calories, :integer
+      add :points, :integer
+
+      timestamps()
+    end
+
+    create index(:workouts, [:gym_rats_user_id])
+    create index(:workouts, [:challenge_id])
+  end
+end
