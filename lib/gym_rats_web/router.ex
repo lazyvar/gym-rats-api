0dc4e3655c46e9586a796d1311b39795d8b00792
Mack Hasz <big.mack.with.pies@gmail.com>
2020-01-26 13:25:22 -0800

    alphabetacal

--- lib/gym_rats_web/router.ex
+++ lib/gym_rats_web/router.ex
@@ -5,33 +5,33 @@ defmodule GymRatsWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/auth", GymRatsWeb, as: :auth do
-    pipe_through :api
-
-    resources "/tokens", Auth.TokenController, only: [:create]
-    resources "/accounts", Auth.AccountController, only: [:create]
-    resources "/passwords", Auth.PasswordController, only: [:update, :create]
-  end
-
   scope "/", GymRatsWeb do
     pipe_through :api
 
-    resources "/accounts", AccountController, only: [:update] do
-      resources "/workouts", Account.WorkoutController, only: [:index]
-    end
-    resources "/memberships", MembershipController, param: "challenge_id", only: [:create, :delete]
+    resources "/accounts", AccountController, only: [:update]
     resources "/challenges", ChallengeController, only: [:create, :index, :update] do
-      resources "/workouts", Challenge.WorkoutController, only: [:index]
       resources "/members", Challenge.MemberController, only: [:index] do
         resources "/workouts", Challenge.Member.WorkoutsController, only: [:index]
       end
       resources "/messages", Challenge.MessageController, only: [:index]
+      resources "/workouts", Challenge.WorkoutController, only: [:index]
     end
+    resources "/comments", CommentController, only: [:delete]
+    resources "/devices", DeviceController, only: [:create, :delete] do
+      resources "/workouts", Account.WorkoutController, only: [:index]
+    end
+    resources "/memberships", MembershipController, param: "challenge_id", only: [:create, :delete]
+    resources "/messages", MessageController, only: [:create]
     resources "/workouts", WorkoutController, only: [:create, :delete] do
       resources "/comments", Workout.CommentController, only: [:create, :index]
     end
-    resources "/comments", CommentController, only: [:delete]
-    resources "/messages", MessageController, only: [:create]
-    resources "/devices", DeviceController, only: [:create, :delete]
+  end
+
+  scope "/auth", GymRatsWeb, as: :auth do
+    pipe_through :api
+
+    resources "/accounts", Auth.AccountController, only: [:create]
+    resources "/passwords", Auth.PasswordController, only: [:update, :create]
+    resources "/tokens", Auth.TokenController, only: [:create]
   end
 end
