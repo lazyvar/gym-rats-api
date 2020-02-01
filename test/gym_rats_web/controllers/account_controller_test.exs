898ec690ec668ec00ef541792cf40690b9106c82
Mack Hasz <big.mack.with.pies@gmail.com>
2020-02-01 09:05:47 -0800

    tdd

---
+++ test/gym_rats_web/controllers/account_controller_test.exs
@@ -0,0 +1,15 @@
+defmodule GymRatsWeb.AccountControllerTest do
+  use GymRatsWeb.ConnCase
+
+  alias GymRats.Model.Account
+
+  describe "create/2" do
+    test "Creates and responds with the newly created user if attributes are valid", %{conn: conn} do
+
+    end
+
+    test "Returns an error and does not create a user if attributes are invalid" do
+
+    end
+  end
+end
