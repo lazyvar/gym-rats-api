22b70d3e09769495d68e225506ae3e85f2d732cb
Mack Hasz <big.mack.with.pies@gmail.com>
2020-02-01 15:35:55 -0800

    delete comment

---
+++ lib/gym_rats_web/controllers/comment_controller.ex
@@ -0,0 +1,22 @@
+defmodule GymRatsWeb.CommentController do
+  use GymRatsWeb, :protected_controller
+  
+  alias GymRats.Model.Comment
+
+  import Ecto.Query
+
+  def delete(conn, %{"id" => id}, account_id) do
+    comment = Comment |> Repo.get(id)
+
+    case comment do
+      nil -> failure(conn, "Comment does not exist")
+      _ ->
+        if comment.gym_rats_user_id != account_id do
+          failure(conn, "You do not have permission to do that.")
+        else
+          comment |> Repo.delete!
+          success(conn, comment)
+        end
+      end
+  end
+end
