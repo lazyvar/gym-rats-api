cd7b615b338213294ef3450d35340392b21e3bd1
Mack Hasz <big.mack.with.pies@gmail.com>
2020-03-23 16:52:09 -0700

    paginate workouts by 100

--- lib/gym_rats_web/controllers/challenges/workout_controller.ex
+++ lib/gym_rats_web/controllers/challenges/workout_controller.ex
@@ -7,13 +7,16 @@ defmodule GymRatsWeb.Challenge.WorkoutController do
 
   import Ecto.Query
 
-  def index(conn, %{"challenge_id" => challenge_id}, _account_id) do
+  def index(conn, %{"challenge_id" => challenge_id, "page" => page}, _account_id) do
+    page = String.to_integer(page)
     challenge_id = String.to_integer(challenge_id)
 
     workouts =
       Workout
       |> join(:left, [w], c in assoc(w, :challenge))
       |> where([w, c], w.challenge_id == ^challenge_id)
+      |> offset(100 * ^page)
+      |> limit(100)
       |> order_by(desc: :created_at)
       |> preload(:account)
       |> Repo.all()
@@ -21,5 +24,5 @@ defmodule GymRatsWeb.Challenge.WorkoutController do
     success(conn, WorkoutView.with_account(workouts))
   end
 
-  def index(conn, _params, _account_id), do: failure(conn, "Missing challenge id.")
+  def index(conn, _params, _account_id), do: failure(conn, "Missing challenge id and page.")
 end
