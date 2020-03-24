ea9709b22323fba2106b56e1baf2482159bba63e
Mack Hasz <mack@hasz.email>
2020-03-24 19:36:16 +0000

    Pigeon config

--- config/prod.exs
+++ config/prod.exs
@@ -27,10 +27,22 @@ config :gym_rats, GymRats.Repo,
   password: System.get_env("DATABASE_PASSWORD") || raise("DATABASE_PASSWORD is missing"),
   database: System.get_env("DATABASE_DATABASE") || raise("DATABASE_DATABASE is missing"),
   hostname: System.get_env("DATABASE_HOSTNAME") || raise("DATABASE_HOSTNAME is missing"),
-  # 10 is good
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 config :logger, level: :info
+
+config :sentry,
+  dsn: System.get_env("SENTRY_DSN"),
+  included_environments: [:prod],
+  environment_name: :prod
+
+config :pigeon, :apns,
+  apns_default: %{
+    key: System.get_env("APNS_TOKEN"),
+    key_identifier: "FW985G67H6",
+    team_id: "ANX28TKVYJ",
+    mode: System.get_env("APNS_MODE") || :dev
+  }
 
 config :gym_rats, GymRats.Mailer,
   adapter: Bamboo.MailgunAdapter,
@@ -39,10 +51,3 @@ config :gym_rats, GymRats.Mailer,
 
 config :joken,
   default_signer: System.get_env("SIGNING_SECRET") || raise("SIGNING_SECRET is missing")
-
-# config :pigeon, :apns,
-#   apns_default: %{
-#     cert: "apns/cert",
-#     key: "apns/key",
-#     mode: System.get_env("APNS_MODE")
-#   }
