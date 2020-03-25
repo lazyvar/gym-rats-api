9525179606d90214f48ba18bdbddd435f829e5d2
Mack Hasz <big.mack.with.pies@gmail.com>
2020-03-24 17:16:04 -0700

    only create extension if in dev, manually do it for prod

--- priv/repo/migrations/20200324234648_create_citext.exs
+++ priv/repo/migrations/20200324234648_create_citext.exs
@@ -2,6 +2,8 @@ defmodule GymRats.Repo.Migrations.CreateCitext do
   use Ecto.Migration
 
   def change do
-  	execute "CREATE EXTENSION IF NOT EXISTS citext"
+  	if Mix.env == :dev do
+	  	execute "CREATE EXTENSION IF NOT EXISTS citext"
+  	end
   end
 end
