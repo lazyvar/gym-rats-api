0d25585ebd25cb923dabbf117659abdabbaad5a2
Mack Hasz <mack@hasz.email>
2020-05-06 12:30:02 -0700

    add message_type to chat_messages

---
+++ priv/repo/migrations/20200506192519_add_message_type_to_chat.exs
@@ -0,0 +1,9 @@
+defmodule GymRats.Repo.Migrations.AddMessageTypeToChat do
+  use Ecto.Migration
+
+  def change do
+		alter table(:chat_messages) do
+			add :message_type, :string, default: "text"
+		end
+  end
+end
