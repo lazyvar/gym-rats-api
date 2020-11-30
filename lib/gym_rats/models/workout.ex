eb7c22756045b845d0c1529dbb95823f6d09a1ee
Mack Hasz <mack@hasz.email>
2020-11-29 16:45:55 -0800

    default occurred_at

--- lib/gym_rats/models/workout.ex
+++ lib/gym_rats/models/workout.ex
@@ -1,5 +1,6 @@
 defmodule GymRats.Model.Workout do
   use Ecto.Schema
+
   import Ecto.Changeset
 
   alias GymRats.Model.{Account, Challenge}
@@ -28,7 +29,7 @@ defmodule GymRats.Model.Workout do
 
   @required ~w(gym_rats_user_id challenge_id title)a
   @optional ~w(
-    steps points steps calories description distance duration google_place_id
+    steps points steps calories description distance duration google_place_id occurred_at
     photo_url apple_device_name apple_source_name apple_workout_uuid activity_type
   )a
 
@@ -41,7 +42,22 @@ defmodule GymRats.Model.Workout do
   def changeset(workout, attrs) do
     workout
     |> cast(attrs, @required ++ @optional)
+    |> add_occurred_at_if_missing
     |> validate_required(@required)
     |> validate_inclusion(:activity_type, @activity_types)
+  end
+
+  defp add_occurred_at_if_missing(%Ecto.Changeset{changes: %{occurred_at: _}} = changeset) do
+    changeset
+  end
+
+  defp add_occurred_at_if_missing(
+         %Ecto.Changeset{data: %GymRats.Model.Workout{occurred_at: nil}} = changeset
+       ) do
+    changeset |> put_change(:occurred_at, DateTime.utc_now())
+  end
+
+  defp add_occurred_at_if_missing(changeset) do
+    changeset
   end
 end
